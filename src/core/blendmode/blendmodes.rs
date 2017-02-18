8d2694d36d1de07edb834473191562e9073d9a9b
Dennis Möhlmann <dmo479@gmail.com>
2017-02-18 12:26:42 +0100

    adds copy blend mode, documents blendmodes

--- src/core/blendmode/blendmodes.rs
+++ src/core/blendmode/blendmodes.rs
@@ -6,6 +6,14 @@
 use glium::draw_parameters::*;
 use super::BlendMode;
 
+/// Replaces source into destination, overwriting color and alpha values.
+pub const COPY: BlendMode = BlendMode(Blend {
+     color: BlendingFunction::AlwaysReplace,
+     alpha: BlendingFunction::AlwaysReplace,
+     constant_value: (0.0, 0.0, 0.0, 0.0)
+});
+
+/// Alpha-blends source into destination.
 pub const ALPHA: BlendMode = BlendMode(Blend {
      color: BlendingFunction::Addition {
          source: LinearBlendingFactor::One,
@@ -18,18 +26,21 @@ pub const ALPHA: BlendMode = BlendMode(Blend {
      constant_value: (0.0, 0.0, 0.0, 0.0)
 });
 
+/// Writes the maximum of source and destination into destination.
 pub const MAX: BlendMode = BlendMode(Blend {
     color: BlendingFunction::Max,
     alpha: BlendingFunction::Max,
     constant_value: (0.0, 0.0, 0.0, 0.0),
 });
 
+/// Writes the minimum of source and destination into destination.
 pub const MIN: BlendMode = BlendMode(Blend {
     color: BlendingFunction::Min,
     alpha: BlendingFunction::Min,
     constant_value: (0.0, 0.0, 0.0, 0.0),
 });
 
+/// Additively lightens destination based on source. !PLACEHOLDER
 pub const LIGHTEN: BlendMode = BlendMode(Blend {
     color: BlendingFunction::Addition {
         source: LinearBlendingFactor::SourceAlpha,
@@ -42,6 +53,7 @@ pub const LIGHTEN: BlendMode = BlendMode(Blend {
     constant_value: (0.0, 0.0, 0.0, 0.0),
 });
 
+/// Additively overlays destination with source. !PLACEHOLDER
 pub const OVERLAY: BlendMode = BlendMode(Blend {
     color: BlendingFunction::Addition {
         source: LinearBlendingFactor::SourceAlpha,
@@ -54,7 +66,7 @@ pub const OVERLAY: BlendMode = BlendMode(Blend {
     constant_value: (0.0, 0.0, 0.0, 0.0),
 });
 
-/// sets the blend function for the layer (like alpha, but adds brightness value)
+/// Sets the blend function for the layer (like alpha, but adds brightness value)
 pub fn alpha_const(brightness: f32) -> BlendMode {
    BlendMode(Blend {
         color: BlendingFunction::Addition {
