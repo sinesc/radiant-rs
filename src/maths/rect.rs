2e7645298d5b4d577715d2bc989a7d3489e25279
Dennis Möhlmann <dmo479@gmail.com>
2017-03-10 07:56:00 +0100

    add top/bottom_left/right methods to Rect

--- src/maths/rect.rs
+++ src/maths/rect.rs
@@ -2,7 +2,7 @@ use prelude::*;
 use maths::Vec2;
 
 /// A 2-dimensional point.
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Default, Debug)]
 pub struct Point2<T = f32>(pub T, pub T) where T: Copy + Clone + Debug;
 
 impl<T> From<(T, T)> for Point2<T> where T: Copy + Clone + Debug {
@@ -30,13 +30,29 @@ impl From<Point2<f32>> for (f32, f32) {
 }
 
 /// A rectangle.
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Default, Debug)]
 pub struct Rect<T: Copy + Clone + Debug = f32>(pub Point2<T>, pub Point2<T>);
 
 impl<T> Rect<T> where T: Copy + Clone + Debug {
     /// Creates a new rectangle with given dimensions.
     pub fn new(x1: T, y1: T, x2: T, y2: T) -> Self {
         Rect(Point2(x1, y1), Point2(x2, y2))
+    }
+    #[inline]
+    pub fn top_left(self: &Self) -> Point2<T> {
+        Point2((self.0).0, (self.0).1)
+    }
+    #[inline]
+    pub fn top_right(self: &Self) -> Point2<T> {
+        Point2((self.1).0, (self.0).1)
+    }
+    #[inline]
+    pub fn bottom_left(self: &Self) -> Point2<T> {
+        Point2((self.0).0, (self.1).1)
+    }
+    #[inline]
+    pub fn bottom_right(self: &Self) -> Point2<T> {
+        Point2((self.1).0, (self.1).1)
     }
 }
 
