9dde18299aa9d529435818fabb1f2616f8a3148e
Dennis Möhlmann <dmo479@gmail.com>
2017-03-25 08:16:07 +0100

    hide window when benchmarking

--- benches/sprite.rs
+++ benches/sprite.rs
@@ -5,10 +5,12 @@ use test::Bencher;
 extern crate radiant_rs;
 use radiant_rs::*;
 
+const SPRITE_ITER: u32 = 100000;
+
 #[bench]
 fn sprite_drawing(b: &mut Bencher) {
 
-    let display = Display::builder().build();
+    let display = Display::builder().hidden().build();
     let renderer = Renderer::new(&display).unwrap();
     let sprite = Sprite::from_file(&renderer.context(), r"res/sprites/ball_v2_32x32x18.jpg").unwrap();
     let layer = Layer::new((640., 480.));
@@ -16,14 +18,16 @@ fn sprite_drawing(b: &mut Bencher) {
     display.clear_frame(Color::black());
 
     // make sure layer is full allocated
-    for i in 0..50000 {
+    for i in 0..SPRITE_ITER {
         sprite.draw(&layer, i, (320., 200.), Color::white());
+        sprite.draw(&layer, i, (0., 0.), Color::red());
     }
 
     b.iter(|| {
         layer.clear();
-        for i in 0..50000 {
+        for i in 0..SPRITE_ITER {
             sprite.draw(&layer, i, (320., 200.), Color::white());
+            sprite.draw(&layer, i, (0., 0.), Color::red());
         }
     });
 
@@ -33,7 +37,7 @@ fn sprite_drawing(b: &mut Bencher) {
 #[bench]
 fn sprite_transformed_drawing(b: &mut Bencher) {
 
-    let display = Display::builder().build();
+    let display = Display::builder().hidden().build();
     let renderer = Renderer::new(&display).unwrap();
     let sprite = Sprite::from_file(&renderer.context(), r"res/sprites/ball_v2_32x32x18.jpg").unwrap();
     let layer = Layer::new((640., 480.));
@@ -41,14 +45,16 @@ fn sprite_transformed_drawing(b: &mut Bencher) {
     display.clear_frame(Color::black());
 
     // make sure layer is full allocated
-    for i in 0..50000 {
+    for i in 0..SPRITE_ITER {
         sprite.draw(&layer, i, (320., 200.), Color::white());
+        sprite.draw(&layer, i, (0., 0.), Color::red());
     }
 
     b.iter(|| {
         layer.clear();
-        for i in 0..50000 {
+        for i in 0..SPRITE_ITER {
             sprite.draw_transformed(&layer, i, (320., 200.), Color::white(), 1.23, (2.34, 3.45));
+            sprite.draw_transformed(&layer, i, (0., 0.), Color::red(), 2.34, (0.67, 0.79));
         }
     });
 
