34c535c38e8adaf47d94947ac6ac1a9cb7eeb071
Dennis Möhlmann <dmo479@gmail.com>
2017-12-21 08:52:29 +0100

    simplify combined example

--- examples/09_combined.rs
+++ examples/09_combined.rs
@@ -1,5 +1,5 @@
 extern crate radiant_rs;
-use radiant_rs::{Display, Renderer, Layer, Sprite, Color, Program, Texture, TextureFilter, utils, blendmodes, postprocessors};
+use radiant_rs::{Display, Renderer, Layer, Sprite, Color, Texture, TextureFilter, utils, blendmodes};
 
 #[path="res/effects/bloom.rs"]
 mod bloom;
@@ -9,56 +9,56 @@ pub fn main() {
     let renderer = Renderer::new(&display).unwrap();
     let sprite = Sprite::from_file(&renderer.context(), r"examples/res/sprites/sparkles_64x64x1.png").unwrap();
     let layer = Layer::new((320., 240.));
-    layer.set_blendmode(blendmodes::LIGHTEN);
+    layer.set_blendmode(blendmodes::SCREEN);
 
     sprite.draw(&layer, 0, (160., 120.), Color::WHITE);
     sprite.draw(&layer, 0, (130., 100.), Color::RED);
     sprite.draw(&layer, 0, (190., 100.), Color::GREEN);
     sprite.draw(&layer, 0, (160., 155.), Color::BLUE);
 
-    // The basic postprocessor previously introduced.
-    let program = Program::from_string(&renderer.context(), include_str!("res/effects/ripple.fs")).unwrap();
-    let ripple_effect = postprocessors::Basic::new(&renderer.context(), program);
-
-    // A custom example bloom effect postprocessor. Here, the arguments define the bloom
-    // quality, bloom spread and brightness.
+    // A custom example bloom effect postprocessor. The arguments define
+    // bloom quality, bloom spread and brightness.
     let bloom_effect = bloom::Bloom::new(&renderer.context(), 2, 5, 10.0);
 
     let surface = Texture::new(&renderer.context(), 640, 480);
     let thumbnail = Texture::new(&renderer.context(), 640, 480);
-    let darken = Color(0., 0., 0., 0.06);
 
     utils::renderloop(|frame| {
-        display.clear_frame(Color::BLACK);
+        display.prepare_frame();
         layer.view_matrix().rotate_at((160., 120.), frame.delta_f32);
         layer.model_matrix().rotate(frame.delta_f32 * 1.1);
 
         // Back up view matrix, then scale it based on elapsed time
         let prev_view_matrix = layer.view_matrix().clone();
-        layer.view_matrix().scale_at((160., 120.), frame.elapsed_f32.sin() + 1.0 * 2.0);
+        layer.view_matrix().scale_at((160., 120.), (frame.elapsed_f32.sin() + 2.) * 0.5);
 
         // This example simply combines rendering to textures with postprocessors.
         renderer.render_to(&surface, || {
-            renderer.postprocess(&bloom_effect, &blendmodes::LIGHTEN, || {
-                renderer.postprocess(&ripple_effect, &blendmodes::LIGHTEN, || {
-                    renderer.render_to(&thumbnail, || {
-                        renderer.clear(Color::TRANSPARENT);
-                        renderer.draw_layer(&layer, 0);
-                    });
-                    renderer.fill().blendmode(blendmodes::ALPHA).color(darken).draw();
-                    renderer.copy_from(&thumbnail, TextureFilter::Linear);
+            renderer.postprocess(&bloom_effect, &blendmodes::ALPHA, || {
+                // Render to thumbnail...
+                renderer.render_to(&thumbnail, || {
+                    renderer.clear(Color::BLACK);
+                    renderer.draw_layer(&layer, 0);
                 });
+                // ...but also copy to current render-target (the postprocessor input)
+                renderer.copy_from(&thumbnail, TextureFilter::Linear);
             });
-            renderer.fill().blendmode(blendmodes::ALPHA).color(darken).draw();
+            renderer.draw_layer(&layer, 0);
         });
 
         // Draw processed texture to display. Also draw the original layer ontop.
-        renderer.copy_from(&surface, TextureFilter::Linear);
-        renderer.draw_layer(&layer, 0);
+        renderer.fill().blendmode(blendmodes::ALPHA).color(Color::alpha_mask(0.15)).draw();
+        renderer.fill().blendmode(blendmodes::SCREEN).texture(&surface).draw();
 
-        // Draw small thumbnails of the intermediate and final surface
+        // Draw small thumbnails of the intermediate and final surface.
+        // Note: copy_* are fast pixel copy operations (no shaders/blending/transforms). Coordinates are in pixels (integers).
         renderer.copy_rect_from(&thumbnail, (0, 0, 640, 480), (512, 288, 128, 96), TextureFilter::Linear);
         renderer.copy_rect_from(&surface, (0, 0, 640, 480), (512, 384, 128, 96), TextureFilter::Linear);
+
+        // Draw color filtered variants of the thumbnail.
+        renderer.rect((469., 384., 43., 32.)).blendmode(blendmodes::ALPHA).texture(&surface).color(Color::RED).draw();
+        renderer.rect((469., 416., 43., 32.)).blendmode(blendmodes::ALPHA).texture(&surface).color(Color::GREEN).draw();
+        renderer.rect((469., 448., 43., 32.)).blendmode(blendmodes::ALPHA).texture(&surface).color(Color::BLUE).draw();
 
         layer.set_view_matrix(prev_view_matrix);
         display.swap_frame();
